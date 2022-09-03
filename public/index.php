a6d630a352ca48747498b52b2cd3784f4819c56b
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-03 16:37:19 +0300

    Migrated to Vite Svelte TS

--- public/index.php
+++ public/index.php
@@ -2,16 +2,12 @@
 <html lang="en">
 
 <head>
-	<meta charset='utf-8'>
-	<meta name='viewport' content='width=device-width,initial-scale=1'>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Portfolio Site</title>
 
-	<title>Svelte app</title>
-
-	<link rel='icon' type='image/png' href='/favicon.png'>
-	<link rel='stylesheet' href='./global.css'>
-	<link rel='stylesheet' href='./build/bundle.css'>
-
-	<script defer src='./build/bundle.js'></script>
+  <script type="module" crossorigin src="./assets/index.e36c4d1f.js"></script>
 </head>
 
 <body>
---
+++ svelte/src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<main>App</main>
+
+<style>
+</style>
