7b199511fe1837a550f0f6918e5d9defb24744c7
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-07 20:38:51 +0300

    Changed on settings of Prettier

--- svelte/.eslintrc.cjs
+++ svelte/.eslintrc.cjs
@@ -1,35 +1,20 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true
-	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
-	],
-	'overrides': [
-	],
-	'parser': '@typescript-eslint/parser',
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'sourceType': 'module'
-	},
-	'plugins': [
-		'@typescript-eslint'
-	],
-	'rules': {
-		'indent': 'off',
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: 'off',
+    'linebreak-style': 'off',
+    quotes: 'off',
+    semi: 'off',
+  },
 }
--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -5,8 +5,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir:  "../public",
+    outDir: '../public',
   },
 })
-
-
