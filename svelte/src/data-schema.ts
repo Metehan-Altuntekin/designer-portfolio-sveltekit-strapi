1b25819ea0547bf8459339750fcabd5c87d081b8
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-07 20:41:28 +0300

    Created the definitions of data schema required for front-end

---
+++ svelte/src/data-schema.ts
@@ -0,0 +1,150 @@
+// This is the schema of data required for front-end to display.
+// key: [min,max] is used to define a string with min and max lengths
+
+export const dataSchema = {
+  owner: {
+    name: [2, 30],
+    surname: [2, 30],
+    logo: URL,
+  },
+
+  skills: [
+    {
+      id: Number || '',
+      name: [2, 20],
+      icon: URL,
+      largeIcon: URL,
+    },
+  ],
+
+  tags: [[2, 20]],
+
+  services: [
+    {
+      id: Number || '',
+      name: [4, 20],
+      icon: URL,
+
+      textPrimary: [100, 200],
+      textSecondary: [200, 400],
+
+      relatedSkills: [[2, 20]],
+      relatedTags: [[2, 20]],
+    },
+  ],
+
+  projects: [
+    // portfolio projects
+    {
+      id: Number,
+      name: [4, 20],
+      thumbnail: URL,
+      images: [URL],
+      videos: [URL],
+      description: [150, 300],
+
+      relatedServices: [[4, 20]],
+      relatedSkills: [[2, 20]],
+      relatedTags: [[2, 20]],
+    },
+  ],
+
+  sections: {
+    header: {
+      nav: {
+        home: [2, 16],
+        portfolio: [2, 16],
+        services: [2, 16],
+        about: [2, 16],
+        contact: [2, 16],
+      },
+    },
+
+    hero: {
+      heading: [35, 60],
+      subheading: [100, 160],
+      actions: {
+        services: [2, 16],
+        portfolio: [2, 16],
+      },
+    },
+
+    services: {
+      title: [4, 16], // Services
+      actions: {
+        getAnOffer: [4, 16],
+        portfolio: [4, 16],
+        cleanFilters: [4, 10],
+      },
+    },
+
+    portfolio: {
+      title: [2, 16], // Portfolio
+      subheading: [10, 50],
+      filters: {
+        service: [2, 12],
+        tags: [2, 12],
+        skills: [2, 12],
+      },
+    },
+
+    about: {
+      title: [4, 16], // About
+      heading: [32, 50],
+      text: [500, 700],
+      image: URL,
+    },
+
+    contact: {
+      title: [2, 16], // Contact
+      heading: [20, 40],
+      availability: {
+        heading: [20, 40],
+        isAvailable: Boolean,
+      },
+      info: {
+        location: {
+          title: [4, 50],
+          link: URL,
+        },
+        email: {
+          title: [4, 12],
+          value: [4, 40],
+        },
+        phone: {
+          title: [4, 12],
+          value: [6, 16],
+        },
+        socialMedia: {
+          title: [4, 12],
+          options: [
+            {
+              title: [2, 20],
+              link: URL,
+              icon: URL,
+            },
+          ],
+        },
+      },
+    },
+
+    friends: {
+      heading: [8, 20],
+      subheading: [15, 40],
+      friendsList: [
+        {
+          name: [6, 16],
+          image: URL,
+          roles: [[6, 24]],
+          link: URL,
+        },
+      ],
+    },
+
+    footer: {
+      copyright: [10, 30],
+      disclaimer: [10, 30],
+      privacyPolicy: [6, 30],
+    },
+  },
+}
