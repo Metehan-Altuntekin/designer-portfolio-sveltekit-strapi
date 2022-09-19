af0df6124a1c6dca975f6aa72d8db6e94f69a46c
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-19 20:01:23 +0300

    services/Skills & store functions
    - sortedProjects
    - filterParams

---
+++ svelte/src/components/services/Skills.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  export let serviceId: number
+  export let skillIds: number[]
+
+  import type { Skill } from '../../types'
+  import skills from '../../stores/skills'
+  import { filterParams, toggleFilterParams } from '../../stores/projects'
+  import type { FilterParams } from '../../stores/projects'
+  import { onMount } from 'svelte'
+
+  function handleSkillSelection(skill: Skill) {
+    toggleFilterParams(serviceId, 'skills', skill.id)
+    console.info($filterParams)
+  }
+</script>
+
+<ul>
+  {#each $skills as skill}
+    {#if skillIds.includes(skill.id)}
+      {@const selected = $filterParams[serviceId].skills.includes(skill.id)}
+      <li on:click={() => handleSkillSelection(skill)}>
+        <img src={skill.largeIcon} alt={skill.name} title={skill.name} class:selected />
+      </li>
+    {/if}
+  {/each}
+</ul>
+
+<style lang="scss">
+  ul {
+    $size: clamp(2rem, 5vh, 4rem);
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: calc($size * 0.1) 0;
+
+    li {
+      img {
+        height: $size;
+
+        filter: brightness(0.4);
+        transition: 0.2s;
+        cursor: pointer;
+
+        &:hover {
+          filter: brightness(0.8);
+        }
+
+        &.selected {
+          filter: brightness(1);
+        }
+      }
+    }
+  }
+</style>
--- svelte/src/stores/projects.ts
+++ svelte/src/stores/projects.ts
@@ -1,9 +1,60 @@
-import type { Readable } from 'svelte/store'
-import { readable } from 'svelte/store'
+import type { Readable, Writable } from 'svelte/store'
+import { readable, writable, derived, get } from 'svelte/store'
 import type { Project } from '../types'
 
 import { CONTENT } from '../config'
 
+// All the projects with no order
 const projects: Readable<Project[]> = readable(CONTENT.projects)
 
 export default projects
+
+// Separate projects based on related services
+
+type SortedProjects = { [key: number]: Project[] }
+
+export const sortedProjects: Readable<SortedProjects> = derived(projects, ($projects) => {
+  return $projects.reduce((prev, curr) => {
+    curr.relatedServices.map((sId) => {
+      prev = {
+        ...prev,
+        [sId]: prev[sId] ? [...prev[sId], curr] : [curr],
+      }
+    })
+    return prev
+  }, {})
+})
+
+// At each service, filter projects related to filterParams
+
+export interface FilterParams {
+  [key: string]: {
+    skills?: number[]
+    tags?: number[]
+  }
+}
+export const filterParams: Writable<FilterParams> = writable(
+  Object.keys(get(sortedProjects)).reduce(
+    (prev, curr) => ({
+      ...prev,
+      [`${curr}`]: { skills: [], tags: [] },
+    }),
+    {}
+  )
+)
+export function toggleFilterParams(serviceId: number, paramType: string, value: number) {
+  filterParams.update((services): FilterParams => {
+    console.info('toggleFilterParams', services[serviceId])
+    return {
+      ...services,
+      [`${serviceId}`]: {
+        ...services[`${serviceId}`],
+        [paramType]: services[`${serviceId}`][paramType].includes(value)
+          ? services[`${serviceId}`][paramType].filter((param) => param !== value)
+          : [...services[`${serviceId}`][paramType], value],
+      },
+    }
+  })
+}
+
+// TODO filtered stores for each service
