1d9886ff03bc09d7cac8a1907e5b4d42e57a85dd
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-24 20:55:09 +0300

    getSkill() & getService() from stores
    To easily find a skill or a service with it's id

--- svelte/src/stores/services.ts
+++ svelte/src/stores/services.ts
@@ -1,9 +1,13 @@
 import type { Readable } from 'svelte/store'
-import { readable } from 'svelte/store'
+import { readable, get } from 'svelte/store'
 import type { Service } from '../types'
 
 import { CONTENT } from '../config'
 
 const services: Readable<Service[]> = readable(CONTENT.services)
+
+export function getService(id: number): Service {
+  return get(services).find((s) => s.id === id)
+}
 
 export default services
--- svelte/src/stores/skills.ts
+++ svelte/src/stores/skills.ts
@@ -1,9 +1,13 @@
 import type { Readable } from 'svelte/store'
-import { readable } from 'svelte/store'
+import { readable, get } from 'svelte/store'
 import type { Skill } from '../types'
 
 import { CONTENT } from '../config'
 
 const skills: Readable<Skill[]> = readable(CONTENT.skills)
+
+export function getSkill(id: number): Skill {
+  return get(skills).find((s) => s.id === id)
+}
 
 export default skills
