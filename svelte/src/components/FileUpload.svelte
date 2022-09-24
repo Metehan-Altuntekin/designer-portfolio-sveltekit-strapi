7c88ec76937141958610013fba7b2749cf840760
Metehan-Altuntekin <metehanaltuntekin@hotmail.com>
2022-09-24 15:41:39 +0300

    Create FileUpload.svelte

---
+++ svelte/src/components/FileUpload.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let input: HTMLInputElement
+
+  let id = `${(Math.random() * 1000000).toFixed()}`
+
+  // $: files = input.files
+</script>
+
+<div>
+  <label for={id} class="button secondary sm">Browse Files</label>
+  <input type="file" {id} bind:this={input} multiple />
+</div>
+
+<style lang="scss">
+  input {
+    display: none;
+  }
+</style>
