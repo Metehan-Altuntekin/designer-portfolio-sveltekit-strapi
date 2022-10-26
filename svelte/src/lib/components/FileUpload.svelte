f5a3be65d65bee17f40870273f4afcb10e8ca6d2
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-10-26 17:21:45 +0300

    FileUpload add, remove functions and styles

--- svelte/src/lib/components/FileUpload.svelte
+++ svelte/src/lib/components/FileUpload.svelte
@@ -1,37 +1,107 @@
 <script lang="ts">
-  export let value: (File | undefined)[] = []
+  import Icon from '@iconify/svelte'
+  export let value: File[] = []
 
   export let files: FileList | null = null
   export let error: string | undefined = undefined
 
-  let input: HTMLInputElement
+  export let MAX_TOTAL_SIZE: number = 1000 * 1000 * 100 // 100MB
+  export let MAX_FILES: number = 5
 
   let id = `${(Math.random() * 1000000).toFixed()}`
 
   // get files from input element and update value
-  $: value = Array.from(files ?? [])
+  $: files, addFiles()
+  function addFiles() {
+    value = [...value, ...Array.from(files ?? []).filter((file) => !value.some((f) => f.name === file.name))]
+  }
+
+  function removeFile(file: File) {
+    value = value.filter((f) => f.name !== file.name)
+  }
 </script>
 
 <div>
-  <label for={id} class="button secondary xs" class:error>Browse Files</label>
-  <input type="file" {id} bind:this={input} multiple bind:files />
-  <div class="files">
-    {#each Array.from(files ?? []) as file (file.name)}
-      <span class="chip">{file.name}</span>
-    {/each}
+  <div class="helper">
+    Max total size can't exceed {MAX_TOTAL_SIZE / 1000 / 1000}MB and max files: {MAX_FILES}
   </div>
+  <input type="file" {id} multiple bind:files disabled={value.length >= MAX_FILES} />
+  <label for={id} class="button secondary xs" class:error>Add Files</label>
+  {#if !!files}
+    <ul class="files-preview" class:error>
+      {#each value as file (file.name)}
+        <li>
+          <span class="chip" title={file.name}
+            >{file.name}
+            <div class="remove-button" on:click={() => removeFile(file)}>
+              <Icon icon="eva:close-fill" />
+            </div>
+          </span>
+        </li>
+      {/each}
+    </ul>
+  {/if}
   {#if error}
     <div class="error-helper">{error}</div>
   {/if}
 </div>
 
 <style lang="scss">
+  .helper {
+    font-size: 0.8rem;
+    color: #aaa;
+    margin-bottom: 0.5rem;
+  }
+
   input {
     display: none;
   }
 
-  label.error {
-    border: 2px solid var(--error-color);
+  input:disabled + label {
+    cursor: not-allowed;
+    filter: contrast(0.4);
+  }
+
+  .files-preview {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding: 1rem 0;
+
+    li > span {
+      max-width: 10em;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      position: relative;
+      .remove-button {
+        visibility: hidden !important;
+        position: absolute;
+        inset: 0 0 0 auto;
+        font-size: 1.6em;
+        cursor: pointer;
+        background-color: #555;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 0.2em;
+
+        &:hover {
+          background-color: #777;
+        }
+      }
+
+      &:hover {
+        .remove-button {
+          visibility: visible !important;
+        }
+      }
+    }
+  }
+
+  .files-preview.error {
+    border: 1px solid red;
   }
 
   .error-helper {
--- svelte/src/lib/components/contact/ContactForm.svelte
+++ svelte/src/lib/components/contact/ContactForm.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { progress, sendMessage, schema, errors } from '$lib/stores/contact'
+  import { progress, sendMessage, schema, errors, FILES_TOTAL_MAX_SIZE } from '$lib/stores/contact'
   import type { Schema } from '$lib/stores/contact'
 
   import IconField from '../IconField.svelte'
@@ -33,10 +33,12 @@
     required
   />
   <IconField bind:value={value.email} label="Email" icon="ic:round-email" error={$errors.email} required />
-  <IconField bind:value={value.tel} label="Phone Number" icon="ic:round-phone" error={$errors.tel} />
+  <IconField bind:value={value.tel} label="Telephone - optional" icon="ic:round-phone" error={$errors.tel} />
   <IconField bind:value={value.message} label="Your Message" textarea rows={5} error={$errors.message} required />
-  <FileUpload bind:value={value.files} error={$errors.files} />
-  <button type="submit" class="button sm">Send Message</button>
+  <FileUpload bind:value={value.files} error={$errors.files} MAX_FILES={5} MAX_TOTAL_SIZE={FILES_TOTAL_MAX_SIZE} />
+  <button type="submit" class="button sm" class:processing={$progress !== 0} disabled={$progress !== 0}
+    >Send Message</button
+  >
 </form>
 
 <style lang="scss">
@@ -55,5 +57,33 @@
     font-size: clamp(1rem, 2.3vw, 1.6rem);
     color: var(--color-primary);
     font-weight: 600;
+  }
+
+  button.processing {
+    cursor: wait;
+    color: transparent;
+    filter: brightness(0.7);
+
+    &:after {
+      content: '';
+      box-sizing: border-box;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width: 20px;
+      height: 20px;
+      margin-top: -10px;
+      margin-left: -10px;
+      border-radius: 50%;
+      border: 3px solid #fff7;
+      border-top-color: #fff;
+      animation: spinner 0.6s linear infinite;
+    }
+  }
+
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
   }
 </style>
