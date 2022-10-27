95734bf961573249845df53b6adecf379728bbe3
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-10-27 17:56:05 +0300

    Notification.svelte styling

--- svelte/src/lib/components/Notification.svelte
+++ svelte/src/lib/components/Notification.svelte
@@ -14,7 +14,7 @@
   $: if (notification.progress) tweenedProgress.set(notification.progress)
 </script>
 
-<div class="notification {notification.type}" transition:fly={{ x: 200 }}>
+<div class="notification {notification.type}" transition:fly={{ x: 200 }} class:progress={!!notification.progressStore}>
   <h3 class="title">{notification.title}</h3>
 
   {#if notification.message}
@@ -32,19 +32,47 @@
 
 <style lang="scss">
   .notification {
+    --width: 350px;
+    --progress-height: 14px;
+
+    --message-size: clamp(1rem, 1.5vw, 1.5rem);
+    --title-size: calc(var(--message-size) * 1.5);
+    --close-button-size: clamp(1.5rem, 1.5vw, 2rem);
+
+    --success-title-color: #0f5132;
+    --success-message-color: #155724;
+    --success-bg-color: #d1e7dd;
+
+    --info-title-color: #0c5460;
+    --info-message-color: #0c5460;
+    --info-bg-color: #d1ecf1;
+
+    --warning-title-color: #664d03;
+    --warning-message-color: #664d03;
+    --warning-bg-color: #fff3cd;
+
+    --error-title-color: #842029;
+    --error-message-color: #842029;
+    --error-bg-color: #f8d7da;
+
+    --progress-color: #006eff;
+    --progress-bg-color: transparent;
+
     position: relative;
     padding: 1rem;
     margin-bottom: 1rem;
+    width: var(--width);
 
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: start;
 
-    border-radius: 0.5rem;
-    background-color: #fff;
+    border-radius: 7px;
+    background-color: var(--info-bg-color);
     box-shadow: 0 0 12px #0007;
     color: #333;
+    overflow: hidden;
 
     font-size: 1.4rem;
     font-weight: 400;
@@ -55,23 +83,69 @@
     z-index: 1;
   }
 
-  .notification .content {
-    flex: 1;
-  }
-
   .notification .title {
     margin: 0;
-    margin-bottom: 0.5rem;
+    margin-bottom: 0.5em;
+    margin-right: var(--close-button-size); // margin to make room for the close button
 
-    font-size: 1.6rem;
+    font-size: var(--title-size);
     font-weight: 600;
-    line-height: 1.4;
-    color: #333;
+    line-height: 1;
+    color: var(--info-title-color);
   }
 
   .notification .message {
-    margin: 0;
-    color: #333;
+    font-size: var(--message-size);
+    // margin-bottom: 1em;
+    color: var(--info-message-color);
+    line-height: 1.05;
+  }
+
+  progress {
+    --color: var(--progress-color);
+    --background: var(--progress-bg-color);
+
+    position: absolute;
+    inset: auto 0 0 0;
+
+    width: 100%;
+    height: var(--progress-height);
+
+    background-color: var(--background);
+
+    &::-webkit-progress-bar {
+      background-color: var(--background);
+    }
+    &::-webkit-progress-value {
+      background-color: var(--color);
+    }
+    &::-moz-progress-bar {
+      background-color: var(--color);
+    }
+    &::-ms-fill {
+      background-color: var(--color);
+    }
+    &::-ms-fill-lower {
+      background-color: var(--background);
+    }
+    &::-ms-fill-upper {
+      background-color: var(--color);
+    }
+    &::-ms-backdrop {
+      background-color: var(--background);
+    }
+    &::-ms-ticks-after {
+      background-color: var(--color);
+    }
+    &::-ms-ticks-before {
+      background-color: var(--color);
+    }
+    &::-ms-tooltip {
+      background-color: var(--color);
+    }
+    &::-ms-track {
+      background-color: var(--background);
+    }
   }
 
   .notification .close {
@@ -86,7 +160,7 @@
     cursor: pointer;
     outline: none;
 
-    font-size: 1.5rem;
+    font-size: var(--close-button-size);
     padding: 0.1em;
 
     :global(*) {
@@ -94,45 +168,65 @@
     }
   }
 
+  .notification.progress {
+    padding-bottom: calc(var(--progress-height) * 2);
+  }
+
+  // -- Type specific styles
+
   // success notifications
   .notification.success {
-    background-color: #e6ffed;
-    color: #2c974b;
-  }
+    background-color: var(--success-bg-color);
+    color: var(--success-message-color);
 
-  .notification.success .title {
-    color: #2c974b;
-  }
+    .title {
+      color: var(--success-title-color);
+    }
 
-  .notification.success .message {
-    color: #2c974b;
-  }
-
-  // error notifications
-  .notification.error {
-    background-color: #fff1f0;
-    color: #f5222d;
-  }
-
-  .notification.error .title {
-    color: #f5222d;
-  }
-
-  .notification.error .message {
-    color: #f5222d;
+    .message {
+      color: var(--success-message-color);
+    }
   }
 
   // info notifications
   .notification.info {
-    background-color: #e6f7ff;
-    color: #1890ff;
+    background-color: var(--info-bg-color);
+    color: var(--info-message-color);
+
+    .title {
+      color: var(--info-title-color);
+    }
+
+    .message {
+      color: var(--info-message-color);
+    }
   }
 
-  .notification.info .title {
-    color: #1890ff;
+  // warning notifications
+  .notification.warning {
+    background-color: var(--warning-bg-color);
+    color: var(--warning-message-color);
+
+    .title {
+      color: var(--warning-title-color);
+    }
+
+    .message {
+      color: var(--warning-message-color);
+    }
   }
 
-  .notification.info .message {
-    color: #1890ff;
+  // error notifications
+  .notification.error {
+    background-color: var(--error-bg-color);
+    color: var(--error-message-color);
+
+    .title {
+      color: var(--error-title-color);
+    }
+
+    .message {
+      color: var(--error-message-color);
+    }
   }
 </style>
