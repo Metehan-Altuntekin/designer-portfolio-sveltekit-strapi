e46e5de47c1ed0106f17573d8534f9865fc07bf0
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-10-28 19:37:28 +0300

    Fix getService() return type error

--- svelte/src/lib/stores/services.ts
+++ svelte/src/lib/stores/services.ts
@@ -6,7 +6,7 @@ import content from './content'
 
 const services: Readable<Service[]> = readable(get(content).services)
 
-export function getService(id: number): Service {
+export function getService(id: number) {
   return get(services).find((s) => s.id === id)
 }
 
