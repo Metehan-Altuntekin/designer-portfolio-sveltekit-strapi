f35568a67542ac7b01355ff248a06a5965a130f7
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-10-28 19:36:17 +0300

    Define type of Content in Content store

--- svelte/src/lib/stores/content.ts
+++ svelte/src/lib/stores/content.ts
@@ -1,7 +1,10 @@
 import type { Readable } from 'svelte/store'
 import { readable } from 'svelte/store'
+
+import type { Content } from '$lib/types'
+
 import contentConfig from '$lib/content'
 
-const content: Readable<any> = readable(contentConfig || {})
+const content: Readable<Content> = readable(contentConfig || {})
 
 export default content
