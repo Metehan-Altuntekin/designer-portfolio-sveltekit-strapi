47824aa956c659a7427b95a6fcb9b4342cb66d3b
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-10-31 18:22:06 +0300

    Locale store in stores/language.ts storing static content

--- svelte/src/lib/stores/language.ts
+++ svelte/src/lib/stores/language.ts
@@ -1,6 +1,13 @@
 // Multi language support system
+// We are using a simple system to support multiple languages
+// All the data is stored in a single object
+// The object is divided into sections
+// The content is being accessed by using this single object
 
-import { type Writable, writable, get } from 'svelte/store'
+import { type Writable, type Readable, writable, derived, get } from 'svelte/store'
+
+import locales from '$lib/locales'
+import { staticContent } from '$lib/schemas'
 
 export const LANGUAGES = {
   en: {
@@ -15,14 +22,24 @@ export const LANGUAGES = {
   },
 }
 
+export const DEFAULT_LANGUAGE = 'en'
+
+//
+
 export const language: Writable<keyof typeof LANGUAGES> = writable('en')
 
 export function setLanguage(lang: keyof typeof LANGUAGES) {
   if (LANGUAGES[lang]) {
     language.set(lang)
+  } else {
+    console.error('Language not found:', lang)
+    language.set(DEFAULT_LANGUAGE)
   }
 }
 
-export function getLanguage() {
-  return get(language)
-}
+//
+
+// constantly update the locale
+export const locale: Readable<staticContent> = derived(language, ($language) => {
+  return locales[$language]
+})
