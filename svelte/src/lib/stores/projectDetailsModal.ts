6ac6293f782444ea753ca1e36540f85bde8e101e
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-11-04 21:17:47 +0300

    Disable document scroll when ProjectDetailsModal is present

--- svelte/src/lib/stores/projectDetailsModal.ts
+++ svelte/src/lib/stores/projectDetailsModal.ts
@@ -9,6 +9,8 @@ export function openModal(project: Project): void {
   selectedProject.set(project)
   window.location.hash = 'project-details' // set hash when project is selected
 
+  document.documentElement.style.overflow = 'hidden' // disable scroll when modal is open
+
   // when hash changes, close modal
   window.onhashchange = (e: HashChangeEvent) => {
     if (e.oldURL.includes('project-details')) closeModal() // close modal if hash is changed from project-details
@@ -17,6 +19,8 @@ export function openModal(project: Project): void {
 
 export function closeModal(): void {
   selectedProject.set(null)
+
+  document.documentElement.style.overflow = 'auto' // enable scroll when modal is closed
 
   // replace url with no hash
   if (window.location.hash.includes('project-details')) {
