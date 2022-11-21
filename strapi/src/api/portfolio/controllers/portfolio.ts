aafc41f249a8c88a915dd918104c2c31a67445ac
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-11-21 14:53:17 +0300

    Strapi portfolio single type model

---
+++ strapi/src/api/portfolio/controllers/portfolio.ts
@@ -0,0 +1,7 @@
+/**
+ * portfolio controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::portfolio.portfolio');
---
+++ strapi/src/api/portfolio/routes/portfolio.ts
@@ -0,0 +1,7 @@
+/**
+ * portfolio router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::portfolio.portfolio');
