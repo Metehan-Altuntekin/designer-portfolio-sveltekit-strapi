1e28557a9ddc1a725bef504808bd252c28d43b18
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-11-21 14:48:28 +0300

    Strapi friend collection type

---
+++ strapi/src/api/friend/controllers/friend.ts
@@ -0,0 +1,7 @@
+/**
+ * friend controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::friend.friend');
---
+++ strapi/src/api/friend/routes/friend.ts
@@ -0,0 +1,7 @@
+/**
+ * friend router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::friend.friend');
