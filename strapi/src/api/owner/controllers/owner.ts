346f6b9da9a2755371d5c50579bbbde65cc7b52a
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-11-21 16:39:40 +0300

    Remove owner single type
    It is unnecessary for owner data to be dynamic

--- strapi/src/api/owner/controllers/owner.ts
+++
@@ -1,7 +0,0 @@
-/**
- * owner controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::owner.owner');
--- strapi/src/api/owner/services/owner.ts
+++
@@ -1,7 +0,0 @@
-/**
- * owner service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::owner.owner');
