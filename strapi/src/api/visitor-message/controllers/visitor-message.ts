0b60b477f1fc1687f0cf9ac99405b5c8aa06c343
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2022-12-12 15:35:24 +0300

    Strapi visitor-message content type
    To use for message action of visitors

---
+++ strapi/src/api/visitor-message/controllers/visitor-message.ts
@@ -0,0 +1,7 @@
+/**
+ * visitor-message controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::visitor-message.visitor-message');
---
+++ strapi/src/api/visitor-message/services/visitor-message.ts
@@ -0,0 +1,7 @@
+/**
+ * visitor-message service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::visitor-message.visitor-message');
