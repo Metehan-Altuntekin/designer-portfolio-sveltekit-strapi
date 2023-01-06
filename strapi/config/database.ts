c83d20a5d74058a80ce0385193a336e0d53d74e3
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-01-06 16:02:04 +0300

    strapi: use postgresql instead of mysql

--- strapi/config/database.ts
+++ strapi/config/database.ts
@@ -1,13 +1,19 @@
+import { parse } from "pg-connection-string";
+const config = parse(process.env.DATABASE_URL);
+
 export default ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: "postgres",
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host || env("DATABASE_HOST", "localhost"),
+      port: config.port || env.int("DATABASE_PORT", 5432),
+      database: config.database || env("DATABASE_NAME", "strapi"),
+      user: config.user || env("DATABASE_USERNAME", "root"),
+      password: config.password || env("DATABASE_PASSWORD", "password"),
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
+    debug: false,
   },
 });
